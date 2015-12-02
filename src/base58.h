b7dc92f29c682c5d385c9f3c65cdeaded8b5bbe0
Crypt Arc <CryptArc@users.noreply.github.com>
2015-12-02 06:14:47 -0800

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 36, // addresses start with F
+        PUBKEY_ADDRESS = 56, // addresses start with P
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
